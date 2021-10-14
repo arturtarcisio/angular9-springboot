d8377af50ddd3537e3f67ade5f9e461d2fe9aad9
Artur Tarcisio <arturtarcisio1@gmail.com>
2021-10-14 15:14:08 -0300

    Backend - Mapeando a entidade Cliente

---
+++ backend/src/main/java/io/arturtcs/clientes/model/entities/Clientes.java
@@ -0,0 +1,30 @@
+package io.arturtcs.clientes.model.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Clientes implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column
+    private LocalDate dataCadastro;
+
+}
