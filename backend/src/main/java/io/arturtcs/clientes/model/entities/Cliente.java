a92268201c20fbe0a570101ca0cf4c03771d309e
Artur Tarcisio <arturtarcisio1@gmail.com>
2021-10-18 17:12:42 -0300

    Atualizando um cliente

--- backend/src/main/java/io/arturtcs/clientes/model/entities/Cliente.java
+++ backend/src/main/java/io/arturtcs/clientes/model/entities/Cliente.java
@@ -28,7 +28,7 @@ public class Cliente implements Serializable {
     private String cpf;
 
     @JsonFormat(pattern = "dd/MM/yyyy")
-    @Column(name = "data_cadastro")
+    @Column(name = "data_cadastro", updatable = false)
     private LocalDate dataCadastro;
 
     @PrePersist
